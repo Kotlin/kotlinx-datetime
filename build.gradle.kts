0ca7826717b258554df6bbb37a9465df9b6ddcae
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-10-17 20:41:03 +0300

    Initial MPP project structure, common and jvm source sets for beginning

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+buildscript {
+    repositories {
+        mavenCentral()
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+    }
+}
+
+plugins {
+    id("kotlinx.team.infra") version "0.1.0-dev-49"
+}
+
+project(":kotlinx-datetime") {
+    pluginManager.apply("kotlin-multiplatform")
+//    pluginManager.apply("maven-publish")
+}
+
+infra {
+    teamcity {
+        bintrayUser = "%env.BINTRAY_USER%"
+        bintrayToken = "%env.BINTRAY_API_KEY%"
+    }
+    publishing {
+        include(":kotlinx-datetime")
+
+        bintray {
+            organization = "kotlin"
+            repository = "kotlinx"
+            library = "kotlinx.datetime"
+            username = findProperty("bintrayUser") as String?
+            password = findProperty("bintrayApiKey") as String?
+        }
+
+        bintrayDev {
+            organization = "kotlin"
+            repository = "kotlin-dev"
+            library = "kotlinx.datetime"
+            username = findProperty("bintrayUser") as String?
+            password = findProperty("bintrayApiKey") as String?
+        }
+    }
+}
+
+fun jdkPath(version: Int): String {
+    fun envOrProperty(name: String): String? = System.getenv(name) ?: findProperty(name) as String?
+
+    return envOrProperty("JDK_$version") ?:
+            version.takeIf { it < 9 }?.let { envOrProperty("JDK_1$version") } ?:
+            error("Specify path to JDK $version in JDK_$version environment variable or Gradle property")
+}
+//val JDK_6 by ext(jdkPath(6))
+val JDK_8 by ext(jdkPath(8))
+
+allprojects {
+    repositories {
+        mavenCentral()
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
+
---
+++ core/build.gradle.kts
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2016-2019 JetBrains s.r.o.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+plugins {
+    id("kotlin-multiplatform")
+    `maven-publish`
+}
+
+base {
+    archivesBaseName = "kotlinx-datetime" // doesn't work
+}
+
+//val JDK_6: String by project
+val JDK_8: String by project
+
+//publishing {
+//    repositories {
+//        maven(url = "${rootProject.buildDir}/maven") {
+//            this.name = "buildLocal2"
+//        }
+//    }
+//}
+
+kotlin {
+    infra {
+//        target("macosX64")
+//        target("linuxX64")
+//        target("mingwX64")
+    }
+
+    jvm {
+        attributes {
+            attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 8)
+        }
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+                jdkHome = JDK_8
+            }
+        }
+
+    }
+
+    /*
+    jvm("jvm6") {
+        this.withJava()
+        attributes {
+            attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 6)
+        }
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.6"
+                jdkHome = JDK_6
+            }
+        }
+    }
+     */
+
+    /*
+    js {
+        nodejs {
+//            testTask { }
+        }
+        compilations.all {
+            kotlinOptions {
+                sourceMap = true
+                moduleKind = "umd"
+                metaInfo = true
+            }
+        }
+//        compilations["main"].apply {
+//            kotlinOptions {
+//                outputFile = "kotlinx-datetime-tmp.js"
+//            }
+//        }
+    }
+    */
+
+    sourceSets.all {
+        kotlin.setSrcDirs(listOf("$name/src"))
+        resources.setSrcDirs(listOf("$name/resources"))
+        languageSettings.apply {
+            //            progressiveMode = true
+            useExperimentalAnnotation("kotlin.Experimental")
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                api("org.jetbrains.kotlin:kotlin-stdlib-common")
+            }
+        }
+
+        commonTest {
+            dependencies {
+                api("org.jetbrains.kotlin:kotlin-test-common")
+                api("org.jetbrains.kotlin:kotlin-test-annotations-common")
+            }
+        }
+
+        /*
+        val jvm6Main by getting {
+            dependencies {
+                api("org.jetbrains.kotlin:kotlin-stdlib")
+                api("org.threeten:threetenbp:1.4.0")
+
+            }
+        }
+        val jvm6Test by getting {
+            dependencies {
+                api("org.jetbrains.kotlin:kotlin-test-junit")
+            }
+        }
+        */
+
+        val jvmMain by getting {
+            dependencies {
+                api("org.jetbrains.kotlin:kotlin-stdlib")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                api("org.jetbrains.kotlin:kotlin-test-junit")
+            }
+        }
+
+        /*
+        val jsMain by getting {
+            dependencies {
+                api("org.jetbrains.kotlin:kotlin-stdlib-js")
+            }
+        }
+
+        val jsTest by getting {
+            dependencies {
+                api("org.jetbrains.kotlin:kotlin-test-js")
+            }
+        }
+        */
+    }
+}
+
+tasks {
+    named("jvmTest", Test::class) {
+        // maxHeapSize = "1024m"
+//        executable = "$JDK_6/bin/java"
+    }
+}
