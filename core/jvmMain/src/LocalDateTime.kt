153bb25d02e6dc43d2927b2318327bd99e7ca3a9
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-11-21 21:21:52 +0300

    Remove arithmetic operations on LocalDateTime

--- core/jvmMain/src/LocalDateTime.kt
+++ core/jvmMain/src/LocalDateTime.kt
@@ -5,10 +5,7 @@
 @file:JvmName("LocalDateTimeJvmKt")
 package kotlinx.datetime
 
-import kotlin.math.sign
-import kotlin.time.*
 import java.time.LocalDateTime as jtLocalDateTime
-import java.time.Period as jtPeriod
 
 
 public actual typealias Month = java.time.Month
@@ -56,52 +53,3 @@ public actual fun Instant.toLocalDateTime(timeZone: TimeZone): LocalDateTime =
 
 public actual fun LocalDateTime.toInstant(timeZone: TimeZone): Instant =
         this.value.atZone(timeZone.zoneId).toInstant().let(::Instant)
-
-actual fun LocalDateTime.plus(value: Long, unit: CalendarUnit): LocalDateTime =
-        when (unit) {
-            CalendarUnit.YEAR -> this.value.plusYears(value)
-            CalendarUnit.MONTH -> this.value.plusMonths(value)
-            CalendarUnit.WEEK -> this.value.plusWeeks(value)
-            CalendarUnit.DAY -> this.value.plusDays(value)
-            CalendarUnit.HOUR -> this.value.plusHours(value)
-            CalendarUnit.MINUTE -> this.value.plusMinutes(value)
-            CalendarUnit.SECOND -> this.value.plusSeconds(value)
-            CalendarUnit.NANOSECOND -> this.value.plusNanos(value)
-        }.let(::LocalDateTime)
-
-actual fun LocalDateTime.plus(value: Int, unit: CalendarUnit): LocalDateTime =
-        plus(value.toLong(), unit)
-
-actual operator fun LocalDateTime.plus(period: CalendarPeriod): LocalDateTime =
-        with(period) {
-            value
-                    .run { if (years != 0 && months == 0) plusYears(years.toLong()) else this }
-                    .run { if (months != 0) plusMonths(years * 12L + months.toLong()) else this }
-                    .run { if (days != 0) plusDays(days.toLong()) else this }
-                    .run { if (hours != 0) plusHours(hours.toLong()) else this }
-                    .run { if (minutes != 0) plusMinutes(minutes.toLong()) else this }
-                    .run { if (seconds != 0L) plusSeconds(seconds) else this }
-                    .run { if (nanoseconds != 0L) plusNanos(nanoseconds) else this }
-        }.let(::LocalDateTime)
-
-@UseExperimental(ExperimentalTime::class)
-actual operator fun LocalDateTime.minus(other: LocalDateTime): CalendarPeriod {
-    val timeNanoDiff = this.value.toLocalTime().toNanoOfDay() - other.value.toLocalTime().toNanoOfDay()
-    val resultSign = this.compareTo(other).sign
-    val borrowDay: Int
-    val resultTime = if (timeNanoDiff * resultSign < 0) {
-        borrowDay = -resultSign
-        timeNanoDiff.nanoseconds + resultSign.days
-    } else {
-        borrowDay = 0
-        timeNanoDiff.nanoseconds
-    }
-
-    val resultPeriod = jtPeriod.between(other.value.toLocalDate(), this.value.toLocalDate().plusDays(borrowDay.toLong()))
-
-    return with(resultPeriod) {
-        resultTime.toComponents { hours, minutes, seconds, nanoseconds ->
-            CalendarPeriod(years = years, months = months, days = days, hours = hours, minutes = minutes, seconds = seconds.toLong(), nanoseconds = nanoseconds.toLong())
-        }
-    }
-}
