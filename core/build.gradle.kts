4cb571796a3c1a9c90911d7f5e5d730234478fa3
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2020-04-17 16:36:43 +0300

    Correct years in copyright profile
    Update copyright headers respectively

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2019 JetBrains s.r.o.
+ * Copyright 2019-2020 JetBrains s.r.o.
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 import java.net.URL
--- core/nativeMain/src/TimeZone.kt
+++ core/nativeMain/src/TimeZone.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 JetBrains s.r.o.
+ * Copyright 2019-2020 JetBrains s.r.o.
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 /* Based on the ThreeTenBp project.
