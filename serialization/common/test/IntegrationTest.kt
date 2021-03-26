b4b29dabee4b338217ae62dca094fb540ca9f09f
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2021-03-26 16:23:28 +0300

    Add a test for manual setting of serializers

--- serialization/common/test/IntegrationTest.kt
+++ serialization/common/test/IntegrationTest.kt
@@ -26,6 +26,8 @@ class IntegrationTest {
         @Contextual val dateTime: LocalDateTime,
         @Contextual val datePeriod: DatePeriod,
         @Contextual val dateTimePeriod: DateTimePeriod,
+        // @Contextual val dayOfWeek: DayOfWeek, // doesn't compile on Native
+        // @Contextual val month: Month,
         )
 
     private val module = SerializersModule {
@@ -34,6 +36,8 @@ class IntegrationTest {
         contextual(LocalDateTimeComponentSerializer)
         contextual(DatePeriodComponentSerializer)
         contextual(DateTimePeriodComponentSerializer)
+        contextual(DayOfWeekSerializer)
+        contextual(MonthSerializer)
     }
 
     private val format = Json { serializersModule = module }
@@ -46,6 +50,8 @@ class IntegrationTest {
             LocalDateTime.parse("2020-01-03T12:59:58.010203045"),
             DatePeriod.parse("P20Y-2M-3D"),
             DateTimePeriod.parse("-P50Y-1M-2DT3H4M5.0123S"),
+            // DayOfWeek.MONDAY, // doesn't compile on Native
+            // Month.DECEMBER,
         )
         val json = """{"instant":{"epochSeconds":1616549370,"nanosecondsOfSecond":123456789},""" +
                 """"date":{"year":2020,"month":1,"day":2},""" +
@@ -63,6 +69,8 @@ class IntegrationTest {
         val dateTime: LocalDateTime,
         val datePeriod: DatePeriod,
         val dateTimePeriod: DateTimePeriod,
+        // val dayOfWeek: DayOfWeek,
+        // val month: Month,
     )
 
     @Test
@@ -73,6 +81,8 @@ class IntegrationTest {
             LocalDateTime.parse("2020-01-03T12:59:58.010203045"),
             DatePeriod.parse("P20Y-2M-3D"),
             DateTimePeriod.parse("-P50Y-1M-2DT3H4M5.0123S"),
+            // DayOfWeek.MONDAY,
+            // Month.DECEMBER,
         )
         val json = "{\"instant\":\"2021-03-24T01:29:30.123456789Z\"," +
                 "\"date\":\"2020-01-02\"," +
@@ -82,5 +92,39 @@ class IntegrationTest {
                 "}"
         assertEquals(dummyValue, Json.decodeFromString(json))
         assertEquals(json, Json.encodeToString(dummyValue))
+    }
+
+    @Serializable
+    data class Dummy3(
+        @Serializable(with = InstantComponentSerializer::class) val instant: Instant,
+        @Serializable(with = LocalDateComponentSerializer::class) val date: LocalDate,
+        @Serializable(with = LocalDateTimeComponentSerializer::class) val dateTime: LocalDateTime,
+        @Serializable(with = DatePeriodComponentSerializer::class) val datePeriod: DatePeriod,
+        @Serializable(with = DateTimePeriodComponentSerializer::class) val dateTimePeriod: DateTimePeriod,
+        @Serializable(with = DayOfWeekSerializer::class) val dayOfWeek: DayOfWeek,
+        @Serializable(with = MonthSerializer::class) val month: Month,
+    )
+
+    @Test
+    fun testExplicitSerializerSpecification() {
+        val dummyValue = Dummy3(
+            Instant.parse("2021-03-24T01:29:30.123456789Z"),
+            LocalDate.parse("2020-01-02"),
+            LocalDateTime.parse("2020-01-03T12:59:58.010203045"),
+            DatePeriod.parse("P20Y-2M-3D"),
+            DateTimePeriod.parse("-P50Y-1M-2DT3H4M5.0123S"),
+            DayOfWeek.MONDAY,
+            Month.DECEMBER,
+        )
+        val json = """{"instant":{"epochSeconds":1616549370,"nanosecondsOfSecond":123456789},""" +
+                """"date":{"year":2020,"month":1,"day":2},""" +
+                """"dateTime":{"year":2020,"month":1,"day":3,"hour":12,"minute":59,"second":58,"nanosecond":10203045},""" +
+                """"datePeriod":{"years":19,"months":10,"days":-3},""" +
+                """"dateTimePeriod":{"years":-49,"months":-11,"days":2,"hours":-3,"minutes":-4,"seconds":-5,"nanoseconds":-12300000},""" +
+                """"dayOfWeek":"MONDAY",""" +
+                """"month":"DECEMBER"""" +
+                """}"""
+        assertEquals(dummyValue, format.decodeFromString(json))
+        assertEquals(json, format.encodeToString(dummyValue))
     }
 }
