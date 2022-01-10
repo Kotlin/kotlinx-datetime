ff32ad0cac29550f434148890660e7004e2d53cc
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-01-10 14:10:34 +0100

    kotlinx.datetime.Instant conversions to and from JS Date (#171)

---
+++ core/js/src/Converters.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2019-2021 JetBrains s.r.o.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+package kotlinx.datetime
+
+import kotlin.js.*
+
+/**
+ * Converts the [Instant] to an instance of JS [Date].
+ *
+ * The conversion is lossy: JS uses millisecond precision to represent dates, and [Instant] allows for nanosecond
+ * resolution.
+ */
+public fun Instant.toJSDate(): Date = Date(milliseconds = toEpochMilliseconds().toDouble())
+
+/**
+ * Converts the JS [Date] to the corresponding [Instant].
+ */
+public fun Date.toKotlinInstant(): Instant = Instant.fromEpochMilliseconds(getTime().toLong())
---
+++ core/js/test/JsConverterTest.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2019-2021 JetBrains s.r.o.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+package kotlinx.datetime.test
+
+import kotlinx.datetime.*
+import kotlin.js.*
+import kotlin.test.*
+
+class JsConverterTest {
+    @Test
+    fun toJSDateTest() {
+        val releaseInstant = "2016-02-15T00:00Z".toInstant()
+        val releaseDate = releaseInstant.toJSDate()
+        assertEquals(2016, releaseDate.getUTCFullYear())
+        assertEquals(1, releaseDate.getUTCMonth())
+        assertEquals(15, releaseDate.getUTCDate())
+    }
+
+    @Test
+    fun toInstantTest() {
+        val kotlinReleaseEpochMilliseconds = 1455494400000
+        val releaseDate = Date(milliseconds = kotlinReleaseEpochMilliseconds)
+        val releaseInstant = "2016-02-15T00:00Z".toInstant()
+        assertEquals(releaseInstant, releaseDate.toKotlinInstant())
+    }
+}
