ac5ec5f057bf94921d826a8f8c345b0f62a43dc7
Peter <peter.attardo@gmail.com>
2022-03-02 15:18:09 -0800

    Add minus operator to DatePeriod and DateTimePeriod

--- core/common/src/DateTimePeriod.kt
+++ core/common/src/DateTimePeriod.kt
@@ -449,6 +449,17 @@ public operator fun DateTimePeriod.plus(other: DateTimePeriod): DateTimePeriod =
 )
 
 /**
+ * Subtracts one [DateTimePeriod] instance from another.
+ *
+ * @throws DateTimeArithmeticException if arithmetic overflow happens.
+ */
+public operator fun DateTimePeriod.minus(other: DateTimePeriod) : DateTimePeriod = buildDateTimePeriod(
+    safeAdd(totalMonths, -other.totalMonths),
+    safeAdd(days, -other.days),
+    safeAdd(totalNanoseconds, -other.totalNanoseconds)
+)
+
+/**
  * Adds two [DatePeriod] instances.
  *
  * @throws DateTimeArithmeticException if arithmetic overflow happens.
@@ -456,4 +467,14 @@ public operator fun DateTimePeriod.plus(other: DateTimePeriod): DateTimePeriod =
 public operator fun DatePeriod.plus(other: DatePeriod): DatePeriod = DatePeriod(
     safeAdd(totalMonths, other.totalMonths),
     safeAdd(days, other.days),
+)
+
+/**
+ * Subtracts one [DatePeriod] instance from another.
+ *
+ * @throws DateTimeArithmeticException if arithmetic overflow happens.
+ */
+public operator fun DatePeriod.minus(other: DatePeriod): DatePeriod = DatePeriod(
+    safeAdd(totalMonths, -other.totalMonths),
+    safeAdd(days, -other.days)
 )
