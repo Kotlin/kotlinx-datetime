0183de1dd00bed9ebd0f3fac56a14e5b9202c489
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2022-04-26 06:12:38 +0300

    Update copyright in recently changed files

--- core/common/src/serializers/LocalTimeSerializers.kt
+++ core/common/src/serializers/LocalTimeSerializers.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 JetBrains s.r.o.
+ * Copyright 2019-2022 JetBrains s.r.o. and contributors.
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 
