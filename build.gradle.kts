e94a54f66d94aa540a6681ac49b93b3ee06ede35
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2023-03-21 13:13:59 +0100

    Add a clarifying comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ allprojects {
         mavenCentral()
     }
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        // outputs the compiler version to logs so we can check whether the train configuration applied
         kotlinOptions.freeCompilerArgs += "-version"
     }
 }
