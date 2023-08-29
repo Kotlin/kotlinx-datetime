0220188c2d8fce3568e0f8e29b464e7441f918ce
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2023-08-29 21:23:53 +0300

    [migration] Kotlin 1.9.20: KT-59859

--- core/darwin/src/Converters.kt
+++ core/darwin/src/Converters.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 
-@file:OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
+@file:OptIn(kotlinx.cinterop.ExperimentalForeignApi::class, kotlinx.cinterop.UnsafeNumber::class)
 
 package kotlinx.datetime
 
