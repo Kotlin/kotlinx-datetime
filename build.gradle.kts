a3ad7a11db616e57b61d3305f7aff8de24b6dce2
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2023-11-16 17:29:12 +0200

    Enable error loglevel for partial linkage messages
    The change is related to building datetime as a Kotlin user project. See QA-1099 for more details.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,4 +27,10 @@ allprojects {
         // outputs the compiler version to logs so we can check whether the train configuration applied
         kotlinOptions.freeCompilerArgs += "-version"
     }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile>().configureEach {
+        compilerOptions { freeCompilerArgs.add("-Xpartial-linkage-loglevel=ERROR") }
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile>().configureEach {
+        compilerOptions { freeCompilerArgs.add("-Xpartial-linkage-loglevel=ERROR") }
+    }
 }
