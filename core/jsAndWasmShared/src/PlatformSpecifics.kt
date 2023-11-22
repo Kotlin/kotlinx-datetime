ff7ea9078515632a957aced7d49a04fd99ac6933
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2023-11-22 14:37:54 +0100

    CODEREVIEW AMMEND

--- core/jsAndWasmShared/src/PlatformSpecifics.kt
+++ core/jsAndWasmShared/src/PlatformSpecifics.kt
@@ -2,12 +2,15 @@
  * Copyright 2019-2023 JetBrains s.r.o. and contributors.
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
-package kotlinx.datetime
+
+package kotlinx.datetime.internal
 
 internal expect fun getAvailableZoneIdsSet(): Set<String>
 
 public expect interface InteropInterface
 
+@OptIn(ExperimentalMultiplatform::class)
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.FILE)
+@OptionalExpectation
 public expect annotation class JsNonModule()
