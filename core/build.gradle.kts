642f0c5f51ba0d90d6447531c0627a4e56ec6812
Oleg Yukhnevich <whyoleg@gmail.com>
2023-11-30 22:33:45 +0200

    Update Gradle dokka configuration to make sure "source" button is visible in all API docs

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -406,6 +406,19 @@ tasks.withType<AbstractDokkaLeafTask>().configureEach {
             matchingRegex.set(".*\\.internal\\..*")
             suppress.set(true)
         }
+
+        if (name.endsWith("Main")) {
+            sourceLink {
+                // sources are located in PLATFORM/src,
+                // where PLATFORM could be jvm, js, darwin, etc
+                // configuration happens upper in the build script
+                val platform = name.dropLast(4)
+                val relPath = rootProject.projectDir.toPath().relativize(projectDir.toPath())
+                localDirectory.set(projectDir.resolve("$platform/src"))
+                remoteUrl.set(URL("https://github.com/kotlin/kotlinx-datetime/tree/master/$relPath/$platform/src"))
+                remoteLineSuffix.set("#L")
+            }
+        }
     }
 }
 
