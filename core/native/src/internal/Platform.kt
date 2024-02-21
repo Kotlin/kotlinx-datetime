2ebd98a57cae28567a5c7cd79d5bfe15e8c022a3
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2024-02-21 16:09:58 +0100

    Use the same currentTime implementation for all Native targets
    It seems like the only reason to use NSDate.date() is to obtain
    an object. The POSIX implementation is more accurate.

---
+++ core/native/src/internal/Platform.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019-2024 JetBrains s.r.o. and contributors.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+package kotlinx.datetime.internal
+
+import kotlinx.cinterop.*
+import kotlinx.datetime.*
+import platform.posix.*
+
+internal expect val systemTzdb: TimeZoneDatabase
+
+internal expect fun currentSystemDefaultZone(): Pair<String, TimeZoneRules?>
+
+@OptIn(ExperimentalForeignApi::class, UnsafeNumber::class)
+internal fun currentTime(): Instant = memScoped {
+    val tm = alloc<timespec>()
+    val error = clock_gettime(CLOCK_REALTIME.convert(), tm.ptr)
+    check(error == 0) { "Error when reading the system clock: ${strerror(errno)?.toKString() ?: "Unknown error"}" }
+    try {
+        require(tm.tv_nsec in 0 until NANOS_PER_ONE)
+        Instant(tm.tv_sec.convert(), tm.tv_nsec.convert())
+    } catch (e: IllegalArgumentException) {
+        throw IllegalStateException("The readings from the system clock (${tm.tv_sec} seconds, ${tm.tv_nsec} nanoseconds) are not representable as an Instant")
+    }
+}
--- core/windows/src/internal/TimeZoneNative.kt
+++ core/windows/src/internal/TimeZoneNative.kt
@@ -3,27 +3,10 @@
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 
-@file:OptIn(ExperimentalForeignApi::class)
 package kotlinx.datetime.internal
-
-import kotlinx.cinterop.*
-import kotlinx.datetime.*
-import platform.posix.*
 
 internal actual val systemTzdb: TimeZoneDatabase get() = tzdbInRegistry
 
 internal actual fun currentSystemDefaultZone(): Pair<String, TimeZoneRules?> = tzdbInRegistry.currentSystemDefault()
-
-internal actual fun currentTime(): Instant = memScoped {
-    val tm = alloc<timespec>()
-    val error = clock_gettime(CLOCK_REALTIME, tm.ptr)
-    check(error == 0) { "Error when reading the system clock: ${strerror(errno)}" }
-    try {
-        require(tm.tv_nsec in 0 until NANOS_PER_ONE)
-        Instant(tm.tv_sec, tm.tv_nsec)
-    } catch (e: IllegalArgumentException) {
-        throw IllegalStateException("The readings from the system clock (${tm.tv_sec} seconds, ${tm.tv_nsec} nanoseconds) are not representable as an Instant")
-    }
-}
 
 private val tzdbInRegistry = TzdbInRegistry()
