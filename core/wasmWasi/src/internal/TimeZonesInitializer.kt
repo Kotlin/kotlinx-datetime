bf47a60116c01028dae3711de2b7ece164e043e3
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2024-03-20 11:01:43 +0100

    AMEND

--- core/wasmWasi/src/internal/TimeZonesInitializer.kt
+++ core/wasmWasi/src/internal/TimeZonesInitializer.kt
@@ -31,32 +31,14 @@ public fun initializeTimeZonesProvider(provider: TimeZonesProvider) {
 @InternalDateTimeApi
 private var timeZonesProvider: TimeZonesProvider? = null
 
-private val utcData get() = byteArrayOf(
-    84,   90,  105,  102,   50,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-    0,    0,    0,    0,    0,    0,    0,    1,    0,    0,    0,    1,    0,    0,    0,    0,
-    0,    0,    0,   84,   90,  105,  102,   50,    0,    0,    0,    0,    0,    0,    0,    0,
-    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    1,    0,    0,    0,    4,    0,
-    0,    0,    0,    0,    0,   85,   84,   67,    0,   10,   85,   84,   67,   48,   10,
-)
-
-private val utcNames get() = setOf("Universal", "Etc/Universal", "Etc/Zulu", "Etc/UCT", "Etc/UTC", "Zulu", "UCT", "UTC")
-
 @OptIn(InternalDateTimeApi::class)
 internal class TzdbOnData: TimeZoneDatabase {
     override fun rulesForId(id: String): TimeZoneRules {
-        val provider = timeZonesProvider
-        val data: ByteArray
-        if (provider != null) {
-            data = provider.zoneDataByName(id)
-        } else {
-            if (id !in utcNames) throw IllegalTimeZoneException("TimeZones are not supported")
-            data = utcData
-        }
+        val data = timeZonesProvider?.zoneDataByName(id)
+            ?: throw IllegalTimeZoneException("TimeZones are not supported")
         return readTzFile(data).toTimeZoneRules()
     }
 
     override fun availableTimeZoneIds(): Set<String> =
-        timeZonesProvider?.getTimeZones() ?: utcNames
+        timeZonesProvider?.getTimeZones() ?: emptySet()
 }
