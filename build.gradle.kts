94fce17cad20b43bf6f7f8c77f2473d3cd9a38d2
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2024-08-08 10:06:53 +0300

    Introduce new infrastructure for builds as a Kotlin user project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
+
 plugins {
     with(libs.plugins) {
         alias(kotlinx.infra)
@@ -35,4 +40,42 @@ kover {
 dependencies {
     kover(project(":kotlinx-datetime"))
     kover(project(":kotlinx-datetime-serialization"))
+}
+
+// !! infrastructure for builds as a Kotlin user project
+// (although some of these settings are just enabled by default because they're not very invasive anyway)
+subprojects {
+    tasks.withType<KotlinCompilationTask<*>>().configureEach {
+        compilerOptions {
+            // output kotlin.git-searchable names of reported diagnostics
+            freeCompilerArgs.add("-Xrender-internal-diagnostic-names")
+            // output reported warnings even in the presence of reported errors
+            freeCompilerArgs.add("-Xreport-all-warnings")
+
+            with(providers) {
+                gradleProperty("kotlin_language_version").orNull?.let { optionalOverridingKotlinLanguageVersion ->
+                    languageVersion = KotlinVersion.fromVersion(optionalOverridingKotlinLanguageVersion)
+                    logger.info(
+                        "[ktDT-as-KUP] Overrode the Kotlin language version with $optionalOverridingKotlinLanguageVersion for '$path'"
+                    )
+                }
+                gradleProperty("kotlin_api_version").orNull?.let { optionalOverridingKotlinApiVersion ->
+                    apiVersion = KotlinVersion.fromVersion(optionalOverridingKotlinApiVersion)
+                    logger.info(
+                        "[ktDT-as-KUP] Overrode the Kotlin API version with $optionalOverridingKotlinApiVersion for '$path'"
+                    )
+                }
+            }
+        }
+    }
+    tasks.withType<KotlinNativeCompile>().configureEach {
+        compilerOptions {
+            freeCompilerArgs.add("-Xpartial-linkage-loglevel=error")
+        }
+    }
+    tasks.withType<Kotlin2JsCompile>().configureEach {
+        compilerOptions {
+            freeCompilerArgs.add("-Xpartial-linkage-loglevel=error")
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,15 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlinx/maven")
+
+        // !! infrastructure for builds as a Kotlin user project
+        val optionalKotlinArtifactsRepo = providers.gradleProperty("kotlin_repo_url").orNull
+        if (optionalKotlinArtifactsRepo != null) {
+            maven(url = optionalKotlinArtifactsRepo)
+            logger.info(
+                "[ktDT-as-KUP] Registered '$optionalKotlinArtifactsRepo' as a plugin Maven repository"
+            )
+        }
     }
 }
 
@@ -10,6 +19,30 @@ gradle.projectsLoaded {
     allprojects {
         repositories {
             mavenCentral()
+
+            // !! infrastructure for builds as a Kotlin user project
+            val optionalKotlinArtifactsRepo = providers.gradleProperty("kotlin_repo_url").orNull
+            if (optionalKotlinArtifactsRepo != null) {
+                maven(url = optionalKotlinArtifactsRepo)
+                logger.info(
+                    "[ktDT-as-KUP] Registered '$optionalKotlinArtifactsRepo' as a dependency Maven repository for '${path}'"
+                )
+            }
+        }
+    }
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            // !! infrastructure for builds as a Kotlin user project
+            val optionalOverridingKotlinVersion = providers.gradleProperty("kotlin_version").orNull
+            if (optionalOverridingKotlinVersion != null) {
+                version("kotlin", optionalOverridingKotlinVersion)
+                logger.info(
+                    "[ktDT-as-KUP] Overrode the Kotlin distribution version with $optionalOverridingKotlinVersion"
+                )
+            }
         }
     }
 }
