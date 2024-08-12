73b7afaa1f8fce598c9782e2c29a890ef4dd640a
Bogdan Mukvich <bogdan.mukvich@jetbrains.com>
2024-08-12 14:23:51 +0200

    Update teamcity commit status publisher settings (#388)

--- .teamcity/additionalConfiguration.kt
+++ .teamcity/additionalConfiguration.kt
@@ -3,19 +3,20 @@
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 
+import jetbrains.buildServer.configs.kotlin.DslContext
 import jetbrains.buildServer.configs.kotlin.Project
 import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
 
 fun Project.additionalConfiguration() {
     knownBuilds.buildAll.features {
         commitStatusPublisher {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
             publisher = github {
                 githubUrl = "https://api.github.com"
-                authType = personalToken {
-                    token = "credentialsJSON:af36802a-ccd4-401b-86b9-0b08d2dfad17"
+                authType = storedToken {
+                    tokenId = "tc_token_id:CID_7db3007c46f7e30124f81ef54591b223:-1:4b5743ff-b95e-41b0-89d6-e9a50d3048db"
                 }
             }
-            param("github_oauth_user", "ilya-g")
         }
     }
 }
