59bd4fd5c3da5c3a5867ad5ed77a41ead5b3459e
Peter Attardo <peter.attardo@gmail.com>
2024-09-04 17:07:56 -0400

    Rework LocalDateRange to match proposed spec

--- core/common/src/LocalDateRange.kt
+++ core/common/src/LocalDateRange.kt
@@ -5,54 +5,31 @@
 
 package kotlinx.datetime
 
-public abstract class LocalDateIterator : Iterator<LocalDate> {
-    final override fun next(): LocalDate = nextLocalDate()
-    public abstract fun nextLocalDate(): LocalDate
-}
+import kotlin.random.Random
+import kotlin.random.nextInt
 
-internal class LocalDateProgressionIterator(first: LocalDate, last: LocalDate, val step: DatePeriod) : LocalDateIterator() {
-    private val finalElement: LocalDate = last
-    private val increasing = step.positive()
-    private var hasNext: Boolean = if (increasing) first <= last else first >= last
-    private var next: LocalDate = if (hasNext) first else finalElement
-
-    override fun hasNext(): Boolean = hasNext
-
-    override fun nextLocalDate(): LocalDate {
-        if(!hasNext) throw NoSuchElementException()
-        val value = next
-        next += step
-        /**
-         * Some [DatePeriod]s with opposite-signed constituent parts can get stuck in an infinite loop rather than progressing toward the far future or far past.
-         * A period of P1M-28D for example, when added to any date in February, will return that same date, thus leading to a loop.
-         */
-        if(next == value) throw IllegalStateException("Progression has hit an infinite loop. Check to ensure that the the values for total months and days in the provided step DatePeriod are not equal and opposite for certain month(s).")
-        if ((increasing && next > finalElement) || (!increasing && next < finalElement)) {
-            hasNext = false
-        }
-        return value
-    }
+internal class LocalDateProgressionIterator(private val iterator: IntIterator) : Iterator<LocalDate> {
+    override fun hasNext(): Boolean = iterator.hasNext()
+    override fun next(): LocalDate = LocalDate.fromEpochDays(iterator.next())
 }
 
 public open class LocalDateProgression
-internal constructor
-    (
-    start: LocalDate,
-    endInclusive: LocalDate,
-    public val step: DatePeriod
-) : Iterable<LocalDate> {
-    init {
-        if(!step.positive() && !step.negative()) throw IllegalArgumentException("Provided step DatePeriod is of size zero (or equivalent over an arbitrarily long timeline)")
-    }
-    public val first: LocalDate = start
-    public val last: LocalDate = endInclusive
+internal constructor(internal val intProgression: IntProgression) : Iterable<LocalDate> {
 
-    override fun iterator(): LocalDateIterator = LocalDateProgressionIterator(first, last, step)
+    internal constructor(
+        start: LocalDate,
+        endInclusive: LocalDate,
+        step: Int
+    ) : this(IntProgression.fromClosedRange(start.toEpochDays(), endInclusive.toEpochDays(), step))
 
-    public open fun isEmpty(): Boolean = if (step.positive()) first > last else first < last
+    public val first: LocalDate = LocalDate.fromEpochDays(intProgression.first)
+    public val last: LocalDate = LocalDate.fromEpochDays(intProgression.last)
 
+    override fun iterator(): Iterator<LocalDate> = LocalDateProgressionIterator(intProgression.iterator())
 
-    override fun toString(): String = if (step.positive()) "$first..$last step $step" else "$first downTo $last step $step"
+    public open fun isEmpty(): Boolean = intProgression.isEmpty()
+
+    override fun toString(): String = if (intProgression.step > 0) "$first..$last step ${intProgression.step}" else "$first downTo $last step ${intProgression.step}"
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -60,28 +37,28 @@ internal constructor
 
         other as LocalDateProgression
 
-        if (first != other.first) return false
-        if (last != other.last) return false
-        if (step != other.step) return false
-
-        return true
+        return intProgression == other.intProgression
     }
 
     override fun hashCode(): Int {
-        var result = first.hashCode()
-        result = 31 * result + last.hashCode()
-        result = 31 * result + step.hashCode()
-        return result
+        return intProgression.hashCode()
     }
 
+
     public companion object {
-        public fun fromClosedRange(rangeStart: LocalDate, rangeEnd: LocalDate, step: DatePeriod): LocalDateProgression = LocalDateProgression(rangeStart, rangeEnd, step)
+        public fun fromClosedRange(
+            rangeStart: LocalDate,
+            rangeEnd: LocalDate,
+            stepValue: Int,
+            stepUnit: DateTimeUnit.DayBased
+        ): LocalDateProgression = LocalDateProgression(rangeStart, rangeEnd, stepValue * stepUnit.days)
     }
 }
 
-public class LocalDateRange(start: LocalDate, endInclusive: LocalDate) : LocalDateProgression(start, endInclusive, DatePeriod(days = 1)), ClosedRange<LocalDate> {
+public class LocalDateRange(start: LocalDate, endInclusive: LocalDate) : LocalDateProgression(start, endInclusive, 1), ClosedRange<LocalDate>, OpenEndRange<LocalDate> {
     override val start: LocalDate get() = first
     override val endInclusive: LocalDate get() = last
+    override val endExclusive: LocalDate get() = endInclusive.plus(1, DateTimeUnit.DAY)
 
     @Suppress("ConvertTwoComparisonsToRangeCheck")
     override fun contains(value: LocalDate): Boolean = first <= value && value <= last
@@ -97,13 +74,45 @@ public class LocalDateRange(start: LocalDate, endInclusive: LocalDate) : LocalDa
     }
 }
 
-/**
- * On an arbitrarily long timeline, the average month will be 30.436875 days long (146097 days over 400 years).
- */
-public fun DatePeriod.positive() : Boolean = totalMonths * 30.436875 + days > 0
-public fun DatePeriod.negative() : Boolean = totalMonths * 30.436875 + days < 0
+public fun LocalDateProgression.first(): LocalDate {
+    if (isEmpty())
+        throw NoSuchElementException("Progression $this is empty.")
+    return this.first
+}
+public fun LocalDateProgression.last(): LocalDate {
+    if (isEmpty())
+        throw NoSuchElementException("Progression $this is empty.")
+    return this.last
+}
+public fun LocalDateProgression.firstOrNull(): LocalDate? = if (isEmpty()) null else this.first
+public fun LocalDateProgression.lastOrNull(): LocalDate? = if (isEmpty()) null else this.last
 
-public infix fun LocalDateProgression.step(step: DatePeriod) : LocalDateProgression = LocalDateProgression.fromClosedRange(first, last, step)
-public infix fun LocalDate.downTo(that: LocalDate) : LocalDateProgression = LocalDateProgression.fromClosedRange(this, that, DatePeriod(days = -1))
+public fun LocalDateProgression.reversed(): LocalDateProgression = LocalDateProgression(intProgression.reversed())
+
+public fun LocalDateProgression.step(value: Int, unit: DateTimeUnit.DayBased) : LocalDateProgression = LocalDateProgression(intProgression.step(value * unit.days))
+public fun LocalDateProgression.step(value: Long, unit: DateTimeUnit.DayBased) : LocalDateProgression = step(value.toInt(), unit)
+
+public infix fun LocalDate.downTo(that: LocalDate) : LocalDateProgression = LocalDateProgression.fromClosedRange(this, that, -1, DateTimeUnit.DAY)
+public infix fun LocalDate.downUntil(that: LocalDate) : LocalDateProgression = downTo(that.plus(1, DateTimeUnit.DAY))
 
 public operator fun LocalDate.rangeTo(that: LocalDate): LocalDateRange = LocalDateRange(this, that)
+public operator fun LocalDate.rangeUntil(that: LocalDate) : LocalDateRange = rangeTo(that.minus(1, DateTimeUnit.DAY))
+
+public fun LocalDateProgression.random(randomIntFunction: (IntRange) -> Int) : LocalDate = intProgression.random(randomIntFunction)
+    .let(LocalDate.Companion::fromEpochDays)
+
+public fun LocalDateProgression.random(random: Random = Random) : LocalDate = intProgression.random(random).let(LocalDate.Companion::fromEpochDays)
+
+public fun LocalDateProgression.randomOrNull(randomIntFunction: (range: IntRange) -> Int) : LocalDate? = intProgression.randomOrNull(randomIntFunction)
+    ?.let(LocalDate.Companion::fromEpochDays)
+
+public fun LocalDateProgression.randomOrNull(random: Random = Random) : LocalDate? = intProgression.randomOrNull(random)
+    ?.let(LocalDate.Companion::fromEpochDays)
+
+public inline fun IntProgression.random(func: (IntRange) -> Int) : Int = func(0..(last - first) / step) * step + first
+
+public fun IntProgression.random(random: Random = Random) : Int = random(random::nextInt)
+
+public inline fun IntProgression.randomOrNull(randomIntFunction: (range: IntRange) -> Int) : Int? = if (isEmpty()) null else random(randomIntFunction)
+
+public fun IntProgression.randomOrNull(random: Random = Random) : Int? = if (isEmpty()) null else random(random)
