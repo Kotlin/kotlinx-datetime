c04b640b0bc28613f5290c35634586da69fb112f
Peter Attardo <peter.attardo@gmail.com>
2024-09-09 17:40:58 -0400

    Samples and documentation for LocalDateRange

--- core/common/src/LocalDateRange.kt
+++ core/common/src/LocalDateRange.kt
@@ -16,6 +16,13 @@ private class LocalDateProgressionIterator(private val iterator: LongIterator) :
     override fun next(): LocalDate = LocalDate.fromEpochDays(iterator.next())
 }
 
+/**
+ * A progression of values of type [LocalDate].
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.progressionWithStep
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.reversedProgression
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.firstAndLast
+ */
 public open class LocalDateProgression
 internal constructor(internal val longProgression: LongProgression) : Collection<LocalDate> {
 
@@ -25,21 +32,49 @@ internal constructor(internal val longProgression: LongProgression) : Collection
         step: Long
     ) : this(LongProgression.fromClosedRange(start.toEpochDaysLong(), endInclusive.toEpochDaysLong(), step))
 
+    /**
+     * Returns the first [LocalDate] of the progression
+     */
     public val first: LocalDate = LocalDate.fromEpochDays(longProgression.first)
 
+    /**
+     * Returns the last [LocalDate] of the progression
+     */
     public val last: LocalDate = LocalDate.fromEpochDays(longProgression.last)
 
+    /**
+     * Returns an [Iterator] that traverses the progression from [first] to [last]
+     */
     override fun iterator(): Iterator<LocalDate> = LocalDateProgressionIterator(longProgression.iterator())
 
+    /**
+     * Returns true iff the progression contains no values.
+     * i.e. [first] < [last] if step is positive, or [first] > [last] if step is negative.
+     */
     public override fun isEmpty(): Boolean = longProgression.isEmpty()
 
+    /**
+     * Returns a string representation of the progression.
+     * Uses the range operator notation if the progression is increasing, and `downTo` if it is decreasing.
+     * The step is referenced in days.
+     */
     override fun toString(): String = if (longProgression.step > 0) "$first..$last step ${longProgression.step}D" else "$first downTo $last step ${longProgression.step}D"
 
+    /**
+     * Returns the number of dates in the progression.
+     * Returns [Int.MAX_VALUE] if the number of dates overflows [Int]
+     */
     override val size: Int
         get() = longProgression.size
 
+    /**
+     * Returns true iff every element in [elements] is a member of the progression.
+     */
     override fun containsAll(elements: Collection<LocalDate>): Boolean = elements.all(::contains)
 
+    /**
+     * Returns true iff [element] is a member of the progression.
+     */
     override fun contains(element: LocalDate): Boolean = longProgression.contains(element.toEpochDaysLong())
 
     override fun equals(other: Any?): Boolean = other is LocalDateProgression && longProgression == other.longProgression
@@ -56,9 +91,25 @@ internal constructor(internal val longProgression: LongProgression) : Collection
     }
 }
 
+/**
+ * A range of values of type [LocalDate].
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.simpleRangeCreation
+ */
 public class LocalDateRange(start: LocalDate, endInclusive: LocalDate) : LocalDateProgression(start, endInclusive, 1), ClosedRange<LocalDate>, OpenEndRange<LocalDate> {
+    /**
+     * Returns the lower bound of the range, inclusive.
+     */
     override val start: LocalDate get() = first
+
+    /**
+     * Returns the upper bound of the range, inclusive.
+     */
     override val endInclusive: LocalDate get() = last
+
+    /**
+     * returns the upper bound of the range, exclusive.
+     */
     @Deprecated(
         "This throws an exception if the exclusive end if not inside " +
                 "the platform-specific boundaries for LocalDate. " +
@@ -70,11 +121,21 @@ public class LocalDateRange(start: LocalDate, endInclusive: LocalDate) : LocalDa
         return endInclusive.plus(1, DateTimeUnit.DAY)
     }
 
+    /**
+     * Returns true iff [value] is contained within the range.
+     * i.e. [value] is between [start] and [endInclusive].
+     */
     @Suppress("ConvertTwoComparisonsToRangeCheck")
     override fun contains(value: LocalDate): Boolean = first <= value && value <= last
 
+    /**
+     * Returns true iff there are no dates contained within the range.
+     */
     override fun isEmpty(): Boolean = first > last
 
+    /**
+     * Returns a string representation of the range using the range operator notation.
+     */
     override fun toString(): String = "$first..$last"
 
     public companion object {
@@ -84,32 +145,115 @@ public class LocalDateRange(start: LocalDate, endInclusive: LocalDate) : LocalDa
     }
 }
 
+/**
+ * Returns the first [LocalDate] of the [LocalDateProgression].
+ *
+ * @throws NoSuchElementException if the progression is empty.
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.firstAndLast
+ */
 public fun LocalDateProgression.first(): LocalDate {
     if (isEmpty())
         throw NoSuchElementException("Progression $this is empty.")
     return this.first
 }
+
+/**
+ * Returns the last [LocalDate] of the [LocalDateProgression].
+ *
+ * @throws NoSuchElementException if the progression is empty.
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.firstAndLast
+ */
 public fun LocalDateProgression.last(): LocalDate {
     if (isEmpty())
         throw NoSuchElementException("Progression $this is empty.")
     return this.last
 }
+
+/**
+ * Returns the first [LocalDate] of the [LocalDateProgression], or null if the progression is empty.
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.firstAndLast
+ */
 public fun LocalDateProgression.firstOrNull(): LocalDate? = if (isEmpty()) null else this.first
 
+/**
+ * Returns the last [LocalDate] of the [LocalDateProgression], or null if the progression is empty.
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.firstAndLast
+ */
 public fun LocalDateProgression.lastOrNull(): LocalDate? = if (isEmpty()) null else this.last
 
+/**
+ * Returns a reversed [LocalDateProgression], i.e. one that goes from [last] to [first].
+ * The sign of the step is switched, in order to reverse the directionality of the progression.
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.reversedProgression
+ */
 public fun LocalDateProgression.reversed(): LocalDateProgression = LocalDateProgression(longProgression.reversed())
 
+/**
+ * Returns a [LocalDateProgression] with the same start and end, but a changed step value.
+ *
+ * **Pitfall**: the value parameter represents the magnitude of the step, not the direction, and therefore must be positive.
+ * Its sign will be matched to the sign of the existing step, in order to maintain the directionality of the progression.
+ * If you wish to switch the direction of the progression, use [LocalDateProgression.reversed]
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.progressionWithStep
+ */
 public fun LocalDateProgression.step(value: Int, unit: DateTimeUnit.DayBased) : LocalDateProgression = step(value.toLong(), unit)
+
+/**
+ * Returns a [LocalDateProgression] with the same start and end, but a changed step value.
+ *
+ * **Pitfall**: the value parameter represents the magnitude of the step, not the direction, and therefore must be positive.
+ * Its sign will be matched to the sign of the existing step, in order to maintain the directionality of the progression.
+ * If you wish to switch the direction of the progression, use [LocalDateProgression.reversed]
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.progressionWithStep
+ */
 public fun LocalDateProgression.step(value: Long, unit: DateTimeUnit.DayBased) : LocalDateProgression = LocalDateProgression(longProgression.step(safeMultiply(value, unit.days.toLong())))
 
+/**
+ * Creates a [LocalDateProgression] from `this` down to [that], inclusive.
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.simpleRangeCreation
+ */
 public infix fun LocalDate.downTo(that: LocalDate) : LocalDateProgression = LocalDateProgression.fromClosedRange(this, that, -1, DateTimeUnit.DAY)
 
+/**
+ * Creates a [LocalDateRange] from `this` to [that], inclusive.
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.simpleRangeCreation
+ */
 public operator fun LocalDate.rangeTo(that: LocalDate): LocalDateRange = LocalDateRange(this, that)
+
+/**
+ * Creates a [LocalDateRange] from `this` to [that], exclusive. i.e. from this to (that - 1 day)
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.simpleRangeCreation
+ */
 public operator fun LocalDate.rangeUntil(that: LocalDate) : LocalDateRange = rangeTo(that.minus(1, DateTimeUnit.DAY))
 
+/**
+ * Returns a random [LocalDate] within the bounds of the [LocalDateProgression].
+ *
+ * Takes the step into account; will not return any value within the range that would be skipped over by the progression.
+ *
+ * @throws IllegalArgumentException if the progression is empty.
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.random
+ */
 public fun LocalDateProgression.random(random: Random = Random) : LocalDate = longProgression.random(random).let(LocalDate.Companion::fromEpochDays)
 
+/**
+ * Returns a random [LocalDate] within the bounds of the [LocalDateProgression] or null if the progression is empty.
+ *
+ * Takes the step into account; will not return any value within the range that would be skipped over by the progression.
+ *
+ * @sample kotlinx.datetime.test.samples.LocalDateRangeSamples.random
+ */
 public fun LocalDateProgression.randomOrNull(random: Random = Random) : LocalDate? = longProgression.randomOrNull(random)
     ?.let(LocalDate.Companion::fromEpochDays)
 
---
+++ core/common/test/samples/LocalDateRangeSamples.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019-2024 JetBrains s.r.o. and contributors.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+package kotlinx.datetime.test.samples
+
+import kotlinx.datetime.*
+import kotlin.random.Random
+import kotlin.test.Test
+
+class LocalDateRangeSamples {
+    val Jan_01_2000 = LocalDate(2000, 1, 1)
+    val Jan_05_2000 = LocalDate(2000, 1, 5)
+    val Jan_06_2000 = LocalDate(2000, 1, 6)
+
+    @Test
+    fun simpleRangeCreation() {
+        // Creating LocalDateRange from LocalDate
+        check(Jan_01_2000..Jan_05_2000 == LocalDateRange(Jan_01_2000, Jan_05_2000))
+        check(Jan_01_2000..<Jan_06_2000 == LocalDateRange(Jan_01_2000, Jan_05_2000))
+        check(Jan_05_2000 downTo Jan_01_2000 == LocalDateProgression(Jan_05_2000, Jan_01_2000, -1))
+    }
+
+    @Test
+    fun progressionWithStep() {
+        // Creating LocalDateProgression with a step size other than 1 day
+        check((Jan_01_2000..Jan_05_2000).step(2, DateTimeUnit.DAY) == LocalDateProgression(Jan_01_2000, Jan_05_2000, 2))
+        check((Jan_05_2000 downTo Jan_01_2000).step(2, DateTimeUnit.DAY) == LocalDateProgression(Jan_05_2000, Jan_01_2000, -2))
+    }
+
+    @Test
+    fun reversedProgression() {
+        // Creating LocalDateProgression and flipping its direction
+        check((Jan_01_2000..Jan_05_2000).reversed() == LocalDateProgression(Jan_05_2000, Jan_01_2000, -1))
+        check((Jan_05_2000 downTo Jan_01_2000).reversed() == LocalDateProgression(Jan_01_2000, Jan_05_2000, 1))
+    }
+
+    @Test
+    fun firstAndLast() {
+        // Getting the first and last elements of a LocalDateProgression
+        check((Jan_01_2000..Jan_05_2000).first() == Jan_01_2000)
+        check((Jan_01_2000..Jan_05_2000).last() == Jan_05_2000)
+        check((Jan_01_2000..Jan_06_2000).step(2, DateTimeUnit.DAY).first() == Jan_01_2000)
+        check((Jan_01_2000..Jan_06_2000).step(2, DateTimeUnit.DAY).last() == Jan_05_2000)
+        check((Jan_05_2000..Jan_01_2000).firstOrNull() == null)
+        check((Jan_05_2000..Jan_01_2000).lastOrNull() == null)
+    }
+
+    @Test
+    fun random() {
+        check((Jan_01_2000..Jan_05_2000).random() in Jan_01_2000..Jan_05_2000)
+        check((Jan_01_2000..Jan_05_2000).random(Random(123456)) in Jan_01_2000..Jan_05_2000)
+        check((Jan_05_2000..Jan_01_2000).randomOrNull() == null)
+        check((Jan_05_2000..Jan_01_2000).randomOrNull(Random(123456)) == null)
+    }
+}
