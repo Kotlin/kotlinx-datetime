0880589a4460c8b532ae08f6b46b58a5f02386f3
Peter Attardo <peter.attardo@gmail.com>
2024-10-04 23:21:33 -0400

    Update LocalDateRangeSamples

--- core/common/test/samples/LocalDateRangeSamples.kt
+++ core/common/test/samples/LocalDateRangeSamples.kt
@@ -10,49 +10,96 @@ import kotlin.random.Random
 import kotlin.test.Test
 
 class LocalDateRangeSamples {
-    val Jan_01_2000 = LocalDate(2000, 1, 1)
-    val Jan_05_2000 = LocalDate(2000, 1, 5)
-    val Jan_06_2000 = LocalDate(2000, 1, 6)
 
     @Test
     fun simpleRangeCreation() {
         // Creating LocalDateRange from LocalDate
-        check(Jan_01_2000..Jan_05_2000 == LocalDateRange(Jan_01_2000, Jan_05_2000))
-        check(Jan_01_2000..<Jan_06_2000 == LocalDateRange(Jan_01_2000, Jan_05_2000))
-        check(Jan_05_2000 downTo Jan_01_2000 == LocalDateProgression(Jan_05_2000, Jan_01_2000, -1))
+        check((LocalDate(2000, 1, 1)..LocalDate(2000, 1, 5)).toList() == listOf(
+            LocalDate(2000, 1, 1),
+            LocalDate(2000, 1, 2),
+            LocalDate(2000, 1, 3),
+            LocalDate(2000, 1, 4),
+            LocalDate(2000, 1, 5)
+        ))
+        check(
+            (LocalDate(2000, 1, 1)..<LocalDate(2000, 1, 6)).toList() == listOf(
+                LocalDate(2000, 1, 1),
+                LocalDate(2000, 1, 2),
+                LocalDate(2000, 1, 3),
+                LocalDate(2000, 1, 4),
+                LocalDate(2000, 1, 5)
+            ))
+        check(
+            (LocalDate(2000, 1, 5) downTo LocalDate(2000, 1, 1)).toList() == listOf(
+                LocalDate(2000, 1, 5),
+                LocalDate(2000, 1, 4),
+                LocalDate(2000, 1, 3),
+                LocalDate(2000, 1, 2),
+                LocalDate(2000, 1, 1)
+            ))
     }
 
     @Test
     fun progressionWithStep() {
         // Creating LocalDateProgression with a step size other than 1 day
-        check((Jan_01_2000..Jan_05_2000).step(2, DateTimeUnit.DAY) == LocalDateProgression(Jan_01_2000, Jan_05_2000, 2))
-        check((Jan_05_2000 downTo Jan_01_2000).step(2, DateTimeUnit.DAY) == LocalDateProgression(Jan_05_2000, Jan_01_2000, -2))
+        check(
+            (LocalDate(2000, 1, 1)..LocalDate(2000, 1, 5)).step(2, DateTimeUnit.DAY).toList() == listOf(
+                LocalDate(2000, 1, 1),
+                LocalDate(2000, 1, 3),
+                LocalDate(2000, 1, 5)
+            ))
+        check(
+            (LocalDate(2000, 1, 5) downTo LocalDate(2000, 1, 1)).step(2, DateTimeUnit.DAY).toList() == listOf(
+                LocalDate(2000, 1, 5),
+                LocalDate(2000, 1, 3),
+                LocalDate(2000, 1, 1)
+            ))
     }
 
     @Test
     fun reversedProgression() {
         // Creating LocalDateProgression and flipping its direction
-        check((Jan_01_2000..Jan_05_2000).reversed() == LocalDateProgression(Jan_05_2000, Jan_01_2000, -1))
-        check((Jan_05_2000 downTo Jan_01_2000).reversed() == LocalDateProgression(Jan_01_2000, Jan_05_2000, 1))
+        check(
+            (LocalDate(2000, 1, 1)..LocalDate(2000, 1, 5)).reversed().toList() == listOf(
+                LocalDate(2000, 1, 5),
+                LocalDate(2000, 1, 4),
+                LocalDate(2000, 1, 3),
+                LocalDate(2000, 1, 2),
+                LocalDate(2000, 1, 1)
+            ))
+        check(
+            (LocalDate(2000, 1, 5) downTo LocalDate(2000, 1, 1)).reversed().toList() == listOf(
+                LocalDate(2000, 1, 1),
+                LocalDate(2000, 1, 2),
+                LocalDate(2000, 1, 3),
+                LocalDate(2000, 1, 4),
+                LocalDate(2000, 1, 5)
+            ))
     }
 
     @Test
     fun firstAndLast() {
         // Getting the first and last elements of a LocalDateProgression
-        check((Jan_01_2000..Jan_05_2000).first() == Jan_01_2000)
-        check((Jan_01_2000..Jan_05_2000).last() == Jan_05_2000)
-        check((Jan_01_2000..Jan_06_2000).step(2, DateTimeUnit.DAY).first() == Jan_01_2000)
-        check((Jan_01_2000..Jan_06_2000).step(2, DateTimeUnit.DAY).last() == Jan_05_2000)
-        check((Jan_05_2000..Jan_01_2000).firstOrNull() == null)
-        check((Jan_05_2000..Jan_01_2000).lastOrNull() == null)
+        check((LocalDate(2000, 1, 1)..LocalDate(2000, 1, 5)).first() == LocalDate(2000, 1, 1))
+        check((LocalDate(2000, 1, 1)..LocalDate(2000, 1, 5)).last() == LocalDate(2000, 1, 5))
+        check((LocalDate(2000, 1, 1)..LocalDate(2000, 1, 6)).step(2, DateTimeUnit.DAY).first() == LocalDate(2000, 1, 1))
+        check((LocalDate(2000, 1, 1)..LocalDate(2000, 1, 6)).step(2, DateTimeUnit.DAY).last() == LocalDate(2000, 1, 5))
+        check((LocalDate(2000, 1, 5)..LocalDate(2000, 1, 1)).firstOrNull() == null)
+        check((LocalDate(2000, 1, 5)..LocalDate(2000, 1, 1)).lastOrNull() == null)
     }
 
     @Test
     fun random() {
         // Getting a random element from a LocalDateProgression
-        check((Jan_01_2000..Jan_05_2000).random() in Jan_01_2000..Jan_05_2000)
-        check((Jan_01_2000..Jan_05_2000).random(Random(123456)) in Jan_01_2000..Jan_05_2000)
-        check((Jan_05_2000..Jan_01_2000).randomOrNull() == null)
-        check((Jan_05_2000..Jan_01_2000).randomOrNull(Random(123456)) == null)
+        check((LocalDate(2000, 1, 1)..LocalDate(2000, 1, 5)).random() in LocalDate(2000, 1, 1)..LocalDate(2000, 1, 5))
+        check(
+            (LocalDate(2000, 1, 1)..LocalDate(2000, 1, 5)).random(Random(123456)) in LocalDate(2000, 1, 1)..LocalDate(
+                2000,
+                1,
+                5
+            )
+        )
+        check((LocalDate(2000, 1, 5)..LocalDate(2000, 1, 1)).randomOrNull() == null)
+        check((LocalDate(2000, 1, 5)..LocalDate(2000, 1, 1)).randomOrNull(Random(123456)) == null)
     }
 }
