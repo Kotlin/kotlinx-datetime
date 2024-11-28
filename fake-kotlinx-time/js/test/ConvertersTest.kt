fb88a097ccac0a0b18bcd5b6450b2f138f41fd53
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2024-11-28 14:34:55 +0100

    Add tests and samples to kotlinx.time

---
+++ fake-kotlinx-time/js/test/ConvertersTest.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2019-2022 JetBrains s.r.o. and contributors.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+package kotlinx.time.test
+
+import kotlinx.time.*
+import kotlin.js.Date
+import kotlin.test.*
+
+class ConvertersTest {
+    @Test
+    fun toJSDateTest() {
+        val releaseInstant = Instant.parse("2016-02-15T00:00:00Z")
+        val releaseDate = releaseInstant.toJSDate()
+        assertEquals(2016, releaseDate.getUTCFullYear())
+        assertEquals(1, releaseDate.getUTCMonth())
+        assertEquals(15, releaseDate.getUTCDate())
+    }
+
+    @Test
+    fun toInstantTest() {
+        val kotlinReleaseEpochMilliseconds = 1455494400000
+        val releaseDate = Date(milliseconds = kotlinReleaseEpochMilliseconds)
+        val releaseInstant = Instant.parse("2016-02-15T00:00:00Z")
+        assertEquals(releaseInstant, releaseDate.toKotlinInstant())
+    }
+}
