d3238e99d48b08c8edca700df56bf83908d55a50
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2024-11-28 13:55:38 +0100

    Copy the commonKotlin Instant implementation to kotlinx.time

---
+++ fake-kotlinx-time/js/src/Platform.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2019-2024 JetBrains s.r.o. and contributors.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+package kotlinx.time
+
+import kotlin.js.Date
+
+internal actual fun currentTime(): Instant = Instant.fromEpochMilliseconds(Date().getTime().toLong())
