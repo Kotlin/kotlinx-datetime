bb1b3bb41a5bb09b97cd5fdaa2922dae752511b4
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2024-11-28 13:01:10 +0100

    Adapt the kotlinx.time classes API for the stdlib

--- fake-kotlinx-time/common/src/Clock.kt
+++ fake-kotlinx-time/common/src/Clock.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 
-package kotlinx.datetime
+package kotlinx.time
 
 import kotlin.time.*
 
@@ -59,70 +59,3 @@ public interface Clock {
 
     }
 }
-
-/**
- * Returns the current date at the given [time zone][timeZone], according to [this Clock][this].
- *
- * The time zone is important because the current date is not the same in all time zones at the same instant.
- *
- * @sample kotlinx.datetime.test.samples.ClockSamples.todayIn
- */
-public fun Clock.todayIn(timeZone: TimeZone): LocalDate =
-    now().toLocalDateTime(timeZone).date
-
-/**
- * Returns a [TimeSource] that uses this [Clock] to mark a time instant and to find the amount of time elapsed since that mark.
- *
- * **Pitfall**: using this function with [Clock.System] is error-prone
- * because [Clock.System] is not well suited for measuring time intervals.
- * Please only use this conversion function on the [Clock] instances that are fully controlled programmatically.
- */
-@ExperimentalTime
-public fun Clock.asTimeSource(): TimeSource.WithComparableMarks = object : TimeSource.WithComparableMarks {
-    override fun markNow(): ComparableTimeMark = InstantTimeMark(now(), this@asTimeSource)
-}
-
-@ExperimentalTime
-private class InstantTimeMark(private val instant: Instant, private val clock: Clock) : ComparableTimeMark {
-    override fun elapsedNow(): Duration = saturatingDiff(clock.now(), instant)
-
-    override fun plus(duration: Duration): ComparableTimeMark = InstantTimeMark(instant.saturatingAdd(duration), clock)
-    override fun minus(duration: Duration): ComparableTimeMark = InstantTimeMark(instant.saturatingAdd(-duration), clock)
-
-    override fun minus(other: ComparableTimeMark): Duration {
-        if (other !is InstantTimeMark || other.clock != this.clock) {
-            throw IllegalArgumentException("Subtracting or comparing time marks from different time sources is not possible: $this and $other")
-        }
-        return saturatingDiff(this.instant, other.instant)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return other is InstantTimeMark && this.clock == other.clock && this.instant == other.instant
-    }
-
-    override fun hashCode(): Int = instant.hashCode()
-
-    override fun toString(): String = "InstantTimeMark($instant, $clock)"
-
-    private fun Instant.isSaturated() = this == Instant.MAX || this == Instant.MIN
-    private fun Instant.saturatingAdd(duration: Duration): Instant {
-        if (isSaturated()) {
-            if (duration.isInfinite() && duration.isPositive() != this.isDistantFuture) {
-                throw IllegalArgumentException("Summing infinities of different signs")
-            }
-            return this
-        }
-        return this + duration
-    }
-    private fun saturatingDiff(instant1: Instant, instant2: Instant): Duration = when {
-        instant1 == instant2 ->
-            Duration.ZERO
-        instant1.isSaturated() || instant2.isSaturated() ->
-            (instant1 - instant2) * Double.POSITIVE_INFINITY
-        else ->
-            instant1 - instant2
-    }
-}
-
-@Deprecated("Use Clock.todayIn instead", ReplaceWith("this.todayIn(timeZone)"), DeprecationLevel.WARNING)
-public fun Clock.todayAt(timeZone: TimeZone): LocalDate = todayIn(timeZone)
--- fake-kotlinx-time/jvm/src/Converters.kt
+++ fake-kotlinx-time/jvm/src/Converters.kt
@@ -3,93 +3,15 @@
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 
-package kotlinx.datetime
+package kotlinx.time
 
 /**
- * Converts this [kotlinx.datetime.Instant][Instant] value to a [java.time.Instant][java.time.Instant] value.
+ * Converts this [kotlin.time.Instant][Instant] value to a [java.time.Instant][java.time.Instant] value.
  */
-public fun Instant.toJavaInstant(): java.time.Instant = this.value
+public fun Instant.toJavaInstant(): java.time.Instant =
+    java.time.Instant.ofEpochSecond(epochSeconds, nanosecondsOfSecond.toLong())
 
 /**
- * Converts this [java.time.Instant][java.time.Instant] value to a [kotlinx.datetime.Instant][Instant] value.
+ * Converts this [java.time.Instant][java.time.Instant] value to a [kotlin.time.Instant][Instant] value.
  */
-public fun java.time.Instant.toKotlinInstant(): Instant = Instant(this)
-
-
-/**
- * Converts this [kotlinx.datetime.LocalDateTime][LocalDateTime] value to a [java.time.LocalDateTime][java.time.LocalDateTime] value.
- */
-public fun LocalDateTime.toJavaLocalDateTime(): java.time.LocalDateTime = this.value
-
-/**
- * Converts this [java.time.LocalDateTime][java.time.LocalDateTime] value to a [kotlinx.datetime.LocalDateTime][LocalDateTime] value.
- */
-public fun java.time.LocalDateTime.toKotlinLocalDateTime(): LocalDateTime = LocalDateTime(this)
-
-/**
- * Converts this [kotlinx.datetime.LocalDateTime][LocalTime] value to a [java.time.LocalTime][java.time.LocalTime] value.
- */
-public fun LocalTime.toJavaLocalTime(): java.time.LocalTime = this.value
-
-/**
- * Converts this [java.time.LocalTime][java.time.LocalTime] value to a [kotlinx.datetime.LocalTime][LocalTime] value.
- */
-public fun java.time.LocalTime.toKotlinLocalTime(): LocalTime = LocalTime(this)
-
-
-/**
- * Converts this [kotlinx.datetime.LocalDate][LocalDate] value to a [java.time.LocalDate][java.time.LocalDate] value.
- */
-public fun LocalDate.toJavaLocalDate(): java.time.LocalDate = this.value
-
-/**
- * Converts this [java.time.LocalDate][java.time.LocalDate] value to a [kotlinx.datetime.LocalDate][LocalDate] value.
- */
-public fun java.time.LocalDate.toKotlinLocalDate(): LocalDate = LocalDate(this)
-
-
-/**
- * Converts this [kotlinx.datetime.DatePeriod][DatePeriod] value to a [java.time.Period][java.time.Period] value.
- */
-public fun DatePeriod.toJavaPeriod(): java.time.Period = java.time.Period.of(this.years, this.months, this.days)
-
-/**
- * Converts this [java.time.Period][java.time.Period] value to a [kotlinx.datetime.DatePeriod][DatePeriod] value.
- */
-public fun java.time.Period.toKotlinDatePeriod(): DatePeriod = DatePeriod(this.years, this.months, this.days)
-
-
-/**
- * Converts this [kotlinx.datetime.TimeZone][TimeZone] value to a [java.time.ZoneId][java.time.ZoneId] value.
- */
-public fun TimeZone.toJavaZoneId(): java.time.ZoneId = this.zoneId
-
-/**
- * Converts this [java.time.ZoneId][java.time.ZoneId] value to a [kotlinx.datetime.TimeZone][TimeZone] value.
- */
-public fun java.time.ZoneId.toKotlinTimeZone(): TimeZone = TimeZone.ofZone(this)
-
-
-/**
- * Converts this [kotlinx.datetime.FixedOffsetTimeZone][FixedOffsetTimeZone] value to a [java.time.ZoneOffset][java.time.ZoneOffset] value.
- */
-public fun FixedOffsetTimeZone.toJavaZoneOffset(): java.time.ZoneOffset = this.offset.zoneOffset
-
-/**
- * Converts this [java.time.ZoneOffset][java.time.ZoneOffset] value to a [kotlinx.datetime.FixedOffsetTimeZone][FixedOffsetTimeZone] value.
- */
-public fun java.time.ZoneOffset.toKotlinFixedOffsetTimeZone(): FixedOffsetTimeZone = FixedOffsetTimeZone(UtcOffset(this))
-
-@Deprecated("Use toKotlinFixedOffsetTimeZone() instead.", ReplaceWith("this.toKotlinFixedOffsetTimeZone()"))
-public fun java.time.ZoneOffset.toKotlinZoneOffset(): FixedOffsetTimeZone = toKotlinFixedOffsetTimeZone()
-
-/**
- * Converts this [kotlinx.datetime.UtcOffset][UtcOffset] value to a [java.time.ZoneOffset][java.time.ZoneOffset] value.
- */
-public fun UtcOffset.toJavaZoneOffset(): java.time.ZoneOffset = this.zoneOffset
-
-/**
- * Converts this [java.time.ZoneOffset][java.time.ZoneOffset] value to a [kotlinx.datetime.UtcOffset][UtcOffset] value.
- */
-public fun java.time.ZoneOffset.toKotlinUtcOffset(): UtcOffset = UtcOffset(this)
-
+public fun java.time.Instant.toKotlinInstant(): Instant = Instant.fromEpochSeconds(epochSecond, nano)
