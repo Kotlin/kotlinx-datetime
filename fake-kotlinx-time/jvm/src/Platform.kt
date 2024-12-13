d8c67e8ebe9dc14a4d9fc532d436d15ae1df6eeb
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2024-12-13 10:14:23 +0100

    Support Android devices with API level < 24

--- fake-kotlinx-time/jvm/src/Platform.kt
+++ fake-kotlinx-time/jvm/src/Platform.kt
@@ -5,4 +5,22 @@
 
 package kotlinx.time
 
-internal actual fun currentTime(): Instant = java.time.Instant.now().toKotlinInstant()
+internal actual fun currentTime(): Instant = if (javaTimeAvailable) {
+    java.time.Instant.now().toKotlinInstant()
+} else {
+    /* After experimenting in Android Studio, it seems like on Android with API < 24, only millisecond precision
+    is available in `Instant.now()` with core library desugaring enabled. Because of that, `currentTimeMillis`
+    is good enough + suggesting that our users enable core library desugaring isn't going to bring any benefits,
+    so the KDoc for [Clock] does not mention any of this. */
+    Instant.fromEpochMilliseconds(System.currentTimeMillis())
+}
+
+/**
+ * `false` for Android devices with API level < 24, where java.time is not available.
+ */
+private val javaTimeAvailable = try {
+    java.time.Instant.now()
+    true
+} catch (e: NoClassDefFoundError) {
+    false
+}
