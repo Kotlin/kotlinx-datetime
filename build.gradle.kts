b0b923b5e952709b9f14491e961325bb01d6d6f9
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2025-01-13 20:02:38 +0200

    [kxDT-as-KUP] Enable validation of binary compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("kotlinx.team.infra") version "0.4.0-dev-81"
     kotlin("multiplatform") apply false
     id("org.jetbrains.kotlinx.kover") version "0.8.0-Beta2"
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.17.0"
 }
 
 infra {
