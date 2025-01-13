f92d697565b62b662f70ceef7aa3fb55f1758bed
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2025-01-13 18:49:41 +0200

    Rework infrastructure for builds as a Kotlin user project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
+
 plugins {
     id("kotlinx.team.infra") version "0.4.0-dev-85"
     kotlin("multiplatform") apply false
@@ -20,23 +26,45 @@ infra {
 val mainJavaToolchainVersion by ext(project.property("java.mainToolchainVersion"))
 val modularJavaToolchainVersion by ext(project.property("java.modularToolchainVersion"))
 
+// !! infrastructure for builds as a Kotlin user project
+val shouldWarningsBeErrors = providers.gradleProperty("kotlin_Werror_override")
+    .map { optionalWErrorOverride ->
+        when (optionalWErrorOverride) {
+            "enable" -> true
+            "disable" -> false
+            else -> error("Unknown value for 'kotlin_Werror_override': $optionalWErrorOverride")
+        }
+    }
+    .getOrElse(false)
+    .also { logger.info("shouldWarningsBeErrors: $it") }
+
 allprojects {
     repositories {
-        addTrainRepositories(project)
         mavenCentral()
+
+        // !! infrastructure for builds as a Kotlin user project
+        val optionalKotlinArtifactsRepo = providers.gradleProperty("kotlin_repo_url").orNull
+        if (optionalKotlinArtifactsRepo != null) {
+            maven(url = optionalKotlinArtifactsRepo)
+            logger.info(
+                "[ktDT-as-KUP] Registered '$optionalKotlinArtifactsRepo' as a dependency Maven repository for '${path}'"
+            )
+        }
     }
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        // outputs the compiler version to logs so we can check whether the train configuration applied
-        kotlinOptions.freeCompilerArgs += "-version"
+}
+
+subprojects {
+    tasks.withType<KotlinCompilationTask<*>>().configureEach {
+        compilerOptions {
+            allWarningsAsErrors.set(shouldWarningsBeErrors)
+        }
     }
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile>().configureEach {
-        compilerOptions { freeCompilerArgs.add("-Xjvm-default=all-compatibility") }
-    }
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile>().configureEach {
-        compilerOptions { freeCompilerArgs.add("-Xpartial-linkage-loglevel=ERROR") }
-    }
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile>().configureEach {
-        compilerOptions { freeCompilerArgs.add("-Xpartial-linkage-loglevel=ERROR") }
+
+    // drop this after migration to 2.2.0
+    tasks.withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            freeCompilerArgs.add("-Xjvm-default=all-compatibility")
+        }
     }
 }
 
@@ -60,4 +88,49 @@ kover {
 dependencies {
     kover(project(":kotlinx-datetime"))
     kover(project(":kotlinx-datetime-serialization"))
+}
+
+// !! infrastructure for builds as a Kotlin user project
+subprojects {
+    tasks.withType<KotlinCompilationTask<*>>().configureEach {
+        compilerOptions {
+            // output reported warnings even in the presence of reported errors
+            freeCompilerArgs.add("-Xreport-all-warnings")
+            // output kotlin.git-searchable names of reported diagnostics
+            freeCompilerArgs.add("-Xrender-internal-diagnostic-names")
+
+            with(providers) {
+                gradleProperty("kotlin_language_version").orNull?.let { optionalOverridingKotlinLV ->
+                    languageVersion.set(KotlinVersion.fromVersion(optionalOverridingKotlinLV))
+                    logger.info(
+                        "[ktDT-as-KUP] Overrode the Kotlin language version with $optionalOverridingKotlinLV for '$path'"
+                    )
+                }
+                gradleProperty("kotlin_api_version").orNull?.let { optionalOverridingKotlinAPIV ->
+                    apiVersion.set(KotlinVersion.fromVersion(optionalOverridingKotlinAPIV))
+                    logger.info(
+                        "[ktDT-as-KUP] Overrode the Kotlin API version with $optionalOverridingKotlinAPIV for '$path'"
+                    )
+                }
+                gradleProperty("kotlin_additional_cli_options").orNull?.let { optionalAdditionalCLIOptions ->
+                    if (optionalAdditionalCLIOptions.isNotBlank()) {
+                        freeCompilerArgs.addAll(optionalAdditionalCLIOptions.split(" "))
+                        logger.info(
+                            "[ktDT-as-KUP] Added the following Kotlin CLI options to '$path': $optionalAdditionalCLIOptions"
+                        )
+                    }
+                }
+            }
+        }
+    }
+    tasks.withType<KotlinNativeCompile>().configureEach {
+        compilerOptions {
+            freeCompilerArgs.add("-Xpartial-linkage-loglevel=error")
+        }
+    }
+    tasks.withType<Kotlin2JsCompile>().configureEach {
+        compilerOptions {
+            freeCompilerArgs.add("-Xpartial-linkage-loglevel=error")
+        }
+    }
 }
