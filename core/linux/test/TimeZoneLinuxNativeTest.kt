403ebf943056d77944b75bd4cea86f83fb752ecc
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-03-20 10:19:11 +0400

    #430: Replace shouldRunTests check to Testcontainers.runIfAvailable

--- core/linux/test/TimeZoneLinuxNativeTest.kt
+++ core/linux/test/TimeZoneLinuxNativeTest.kt
@@ -5,26 +5,13 @@
 
 package kotlinx.datetime.test
 
-import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.toKString
 import kotlinx.datetime.*
 import kotlin.test.*
-import platform.posix.getenv
 
 class TimeZoneLinuxNativeTest {
 
-    private var shouldRunTests = false
-
-    @OptIn(ExperimentalForeignApi::class)
-    @BeforeTest
-    fun setup() {
-        shouldRunTests = getenv("INSIDE_TESTCONTAINERS")?.toKString() != null
-    }
-
     @Test
-    fun defaultTimeZoneTest() {
-        if (!shouldRunTests) return
-
+    fun defaultTimeZoneTest() = Testcontainers.runIfAvailable {
         val tz = TimeZone.currentSystemDefault()
         println("TIMEZONE: $tz")
         assertEquals(TimeZone.of("Europe/Oslo"), tz)
