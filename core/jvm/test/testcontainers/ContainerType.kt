95dd6d987521c4a0c9a1e5bf9f9d3b2e1a0cdb56
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-03-24 11:57:52 +0400

    #430: Refactor dockerfiles

--- core/jvm/test/testcontainers/ContainerType.kt
+++ core/jvm/test/testcontainers/ContainerType.kt
@@ -8,28 +8,28 @@ package testcontainers
 enum class ContainerType(val dockerfilePath: String, val imageName: String) {
     // Standard Jessie with Arctic/Longyearbyen timezone
     DEBIAN_JESSIE_CORRECT(
-        "./jvm/test/testcontainers/debian-jessie/correct-config/Dockerfile",
+        "./jvm/test/testcontainers/debian-jessie/correct-config.dockerfile",
         "debian-jessie-timezone-test"
     ),
 
     DEBIAN_JESSIE_DEFAULT(
-        "./jvm/test/testcontainers/debian-jessie/default-config/Dockerfile",
+        "./jvm/test/testcontainers/debian-jessie/default-config.dockerfile",
         "debian-jessie-timezone-test-default"
     ),
 
     DEBIAN_JESSIE_MISSING_LOCALTIME(
-        "./jvm/test/testcontainers/debian-jessie/missing-localtime/Dockerfile",
+        "./jvm/test/testcontainers/debian-jessie/missing-localtime.dockerfile",
         "debian-jessie-timezone-test-missing-localtime"
     ),
 
     // Standard Noble with Arctic/Longyearbyen timezone
     UBUNTU_NOBLE_CORRECT(
-        "./jvm/test/testcontainers/ubuntu-noble/correct-config/Dockerfile",
+        "./jvm/test/testcontainers/ubuntu-noble/correct-config.dockerfile",
         "ubuntu-noble-timezone-test"
     ),
 
     UBUNTU_NOBLE_DEFAULT(
-        "./jvm/test/testcontainers/ubuntu-noble/default-config/Dockerfile",
+        "./jvm/test/testcontainers/ubuntu-noble/default-config.dockerfile",
         "ubuntu-noble-timezone-test-default"
     )
 }
