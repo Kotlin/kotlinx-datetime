09a838a3a8ee1683153199b2b92548d0d50ac6d7
Dmitry Khalanskiy <dmitry.khalanskiy@jetbrains.com>
2025-03-31 15:22:42 +0300

    Introduce a test for using kotlinx-datetime with JPMS

---
+++ jpms-test/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    api(project(":kotlinx-datetime"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,4 +26,6 @@ include(":js-without-timezones")
 project(":js-without-timezones").name = "kotlinx-datetime-js-test-without-timezones"
 include(":js-with-timezones")
 project(":js-with-timezones").name = "kotlinx-datetime-js-test-with-timezones"
+include(":jpms-test")
+project(":jpms-test").name = "kotlinx-datetime-test-with-jpms"
 include(":benchmarks")
