8d17ebb787fce003bbeed4005f6ee8ce884b10b1
Luc Girardin <luc.girardin@macrofocus.com>
2025-04-14 22:23:06 +0200

    Corrected LocalTime.Formats.ISO_BASIC with leading 'T'

--- core/common/test/samples/LocalTimeSamples.kt
+++ core/common/test/samples/LocalTimeSamples.kt
@@ -286,12 +286,12 @@ class LocalTimeSamples {
             val timeWithNanoseconds = LocalTime(hour = 8, minute = 30, second = 15, nanosecond = 160_000_000)
             val timeWithSeconds = LocalTime(hour = 8, minute = 30, second = 15)
             val timeWithoutSeconds = LocalTime(hour = 8, minute = 30)
-            check(LocalTime.Formats.ISO_BASIC.parse("083015.16") == timeWithNanoseconds)
-            check(LocalTime.Formats.ISO_BASIC.parse("083015") == timeWithSeconds)
-            check(LocalTime.Formats.ISO_BASIC.parse("0830") == timeWithoutSeconds)
-            check(LocalTime.Formats.ISO_BASIC.format(timeWithNanoseconds) == "083015.16")
-            check(LocalTime.Formats.ISO_BASIC.format(timeWithSeconds) == "083015")
-            check(LocalTime.Formats.ISO_BASIC.format(timeWithoutSeconds) == "083000")
+            check(LocalTime.Formats.ISO_BASIC.parse("T083015.16") == timeWithNanoseconds)
+            check(LocalTime.Formats.ISO_BASIC.parse("T083015") == timeWithSeconds)
+            check(LocalTime.Formats.ISO_BASIC.parse("T0830") == timeWithoutSeconds)
+            check(LocalTime.Formats.ISO_BASIC.format(timeWithNanoseconds) == "T083015.16")
+            check(LocalTime.Formats.ISO_BASIC.format(timeWithSeconds) == "T083015")
+            check(LocalTime.Formats.ISO_BASIC.format(timeWithoutSeconds) == "T083000")
         }
 
         @Test
