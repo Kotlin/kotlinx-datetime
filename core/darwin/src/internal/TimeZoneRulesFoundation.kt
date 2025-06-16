d058ef506a58fbca38cedcb64363ca16eaacaf8e
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-06-16 16:46:04 +0400

    #485: Add TimeZoneRulesFoundation implementation skeleton to core

---
+++ core/darwin/src/internal/TimeZoneRulesFoundation.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2019-2025 JetBrains s.r.o. and contributors.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+package kotlinx.datetime.internal
+
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.UtcOffset
+
+internal class TimeZoneRulesFoundation : TimeZoneRules {
+    override fun infoAtInstant(instant: Instant): UtcOffset {
+        TODO("Not yet implemented")
+    }
+
+    override fun infoAtDatetime(localDateTime: LocalDateTime): OffsetInfo {
+        TODO("Not yet implemented")
+    }
+}
