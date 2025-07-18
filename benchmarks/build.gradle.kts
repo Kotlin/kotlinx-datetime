72e818b511930cd38b2419084ca094d60ceddf86
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2025-07-18 14:39:47 +0300

    fixup! Rework infrastructure for builds as a Kotlin user project

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -1,9 +1,9 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
-
 /*
  * Copyright 2019-2023 JetBrains s.r.o. and contributors.
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 plugins {
     id("kotlin")
