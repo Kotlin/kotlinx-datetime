4a1f53cf8431bc53afbaee050b377dd7c98d7d39
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-11-19 19:37:37 +0400

    Refactor `PythonDateTimeFormatBenchmark` into `CommonFormats`, update benchmarking parameters, and add ISO DateTime format benchmark.

--- benchmarks/src/jmh/kotlin/CommonFormats.kt
+++ benchmarks/src/jmh/kotlin/CommonFormats.kt
@@ -7,19 +7,20 @@
 
 package kotlinx.datetime
 
+import kotlinx.datetime.format.alternativeParsing
 import kotlinx.datetime.format.char
 import kotlinx.datetime.format.optional
 import org.openjdk.jmh.annotations.*
 import org.openjdk.jmh.infra.Blackhole
 import java.util.concurrent.*
 
-@Warmup(iterations = 5, time = 1)
-@Measurement(iterations = 5, time = 1)
+@Warmup(iterations = 20, time = 2)
+@Measurement(iterations = 30, time = 2)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
-@Fork(1)
-open class PythonDateTimeFormatBenchmark {
+@Fork(2)
+open class CommonFormats {
 
     @Benchmark
     fun buildPythonDateTimeFormat(blackhole: Blackhole) {
@@ -43,5 +44,33 @@ open class PythonDateTimeFormatBenchmark {
             }
         }
         blackhole.consume(v)
+    }
+
+    @Benchmark
+    fun buildIsoDateTimeFormat(blackhole: Blackhole) {
+        val format = LocalDateTime.Format {
+            date(LocalDate.Format {
+                year()
+                char('-')
+                monthNumber()
+                char('-')
+                day()
+            })
+            alternativeParsing({ char('t') }) { char('T') }
+            time(LocalTime.Format {
+                hour()
+                char(':')
+                minute()
+                alternativeParsing({}) {
+                    char(':')
+                    second()
+                    optional {
+                        char('.')
+                        secondFraction(1, 9)
+                    }
+                }
+            })
+        }
+        blackhole.consume(format)
     }
 }
