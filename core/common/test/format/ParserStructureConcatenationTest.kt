4f3ac206822cf5fc41bc3d3b9bc0478fa26e8373
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-03 19:34:34 +0400

    Add test for `concat` to verify distribution of `UnconditionalModification` after `NumberSpanParserOperation`.

--- core/common/test/format/ParserStructureConcatenationTest.kt
+++ core/common/test/format/ParserStructureConcatenationTest.kt
@@ -8,12 +8,45 @@ package kotlinx.datetime.test.format
 import kotlinx.datetime.internal.format.parser.ConstantNumberConsumer
 import kotlinx.datetime.internal.format.parser.NumberSpanParserOperation
 import kotlinx.datetime.internal.format.parser.ParserStructure
+import kotlinx.datetime.internal.format.parser.UnconditionalModification
 import kotlinx.datetime.internal.format.parser.concat
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class ParserStructureConcatenationTest {
+
+    @Test
+    fun concatDistributesUnconditionalModificationAfterNumberSpanParserOperation() {
+        val parser = ParserStructure<Int>(
+            operations = listOf(
+                UnconditionalModification { }
+            ),
+            followedBy = listOf(
+                ParserStructure(
+                    operations = listOf(
+                        NumberSpanParserOperation(listOf(ConstantNumberConsumer("34")))
+                    ),
+                    followedBy = listOf()
+                )
+            )
+        )
+
+        val actual = listOf(parser).concat()
+
+        with(actual) {
+            assertTrue(operations.isEmpty())
+            with(followedBy) {
+                assertEquals(1, size)
+                with(followedBy[0]) {
+                    assertEquals(2, operations.size)
+                    assertTrue(operations[0] is NumberSpanParserOperation)
+                    assertTrue(operations[1] is UnconditionalModification)
+                    assertTrue(followedBy.isEmpty())
+                }
+            }
+        }
+    }
 
     @Test
     fun concatFlattensOperations() {
