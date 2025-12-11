cbc31d9e12cb0de344647c78901859b31bc88460
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-11 15:22:19 +0400

    The performance of creating new datetime formats can be improved (#585)
    - Refactor `ParserStructure.concat` to streamline operation merging logic
    - Add `unconditionalModifications` promotion optimization
    - Add JMH benchmarks for common datetime formats (ISO, RFC 1123, UTC offset)
    - Add `ParserStructureConcatenationTest` for concat behavior verification
    
    Fixes #571

---
+++ benchmarks/src/jmh/kotlin/ParallelFormatBenchmark.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2019-2025 JetBrains s.r.o. and contributors.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+@file:Suppress("unused")
+
+package kotlinx.datetime
+
+import kotlinx.datetime.format.alternativeParsing
+import kotlinx.datetime.format.char
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import java.util.concurrent.TimeUnit
+
+@Warmup(iterations = 10, time = 2)
+@Measurement(iterations = 20, time = 2)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+@Fork(1)
+open class ParallelFormatBenchmark {
+
+    @Param("2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+    var n = 0
+
+    @Benchmark
+    fun formatCreationWithAlternativeParsing(blackhole: Blackhole) {
+        val format = LocalDateTime.Format {
+            repeat(n) {
+                alternativeParsing(
+                    { monthNumber() },
+                    { day() },
+                    primaryFormat = { hour() }
+                )
+                char('@')
+                minute()
+                char('#')
+                second()
+            }
+        }
+        blackhole.consume(format)
+    }
+
+    @Benchmark
+    fun formatCreationWithNestedAlternativeParsing(blackhole: Blackhole) {
+        val format = LocalDateTime.Format {
+            repeat(n) { index ->
+                alternativeParsing(
+                    { monthNumber(); char('-'); day() },
+                    { day(); char('/'); monthNumber() },
+                    primaryFormat = { year(); char('-'); monthNumber(); char('-'); day() }
+                )
+
+                if (index and 1 == 0) {
+                    alternativeParsing(
+                        {
+                            alternativeParsing(
+                                { hour(); char(':'); minute() },
+                                { minute(); char(':'); second() },
+                                primaryFormat = { hour(); char(':'); minute(); char(':'); second() }
+                            )
+                        },
+                        primaryFormat = {
+                            year(); char('-'); monthNumber(); char('-'); day()
+                            char('T')
+                            hour(); char(':'); minute(); char(':'); second()
+                        }
+                    )
+                }
+
+                char('|')
+                if (index % 3 == 0) {
+                    char('|')
+                }
+
+                if (index and 2 == 0) {
+                    alternativeParsing(
+                        { char('Z') },
+                        { char('+'); hour(); char(':'); minute() },
+                        primaryFormat = { char('-'); hour(); char(':'); minute() }
+                    )
+                }
+            }
+        }
+        blackhole.consume(format)
+    }
+}
---
+++ benchmarks/src/jmh/kotlin/SerialFormatBenchmark.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019-2025 JetBrains s.r.o. and contributors.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+@file:Suppress("unused")
+
+package kotlinx.datetime
+
+import kotlinx.datetime.format.char
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import java.util.concurrent.TimeUnit
+
+@Warmup(iterations = 10, time = 2)
+@Measurement(iterations = 20, time = 2)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+@Fork(1)
+open class SerialFormatBenchmark {
+
+    @Param("1", "2", "4", "8", "16", "32", "64", "128", "256", "512", "1024")
+    var n = 0
+
+    @Benchmark
+    fun largeSerialFormat(blackhole: Blackhole) {
+        val format = LocalDateTime.Format {
+            repeat(n) {
+                char('^')
+                monthNumber()
+                char('&')
+                day()
+                char('!')
+                hour()
+                char('$')
+                minute()
+                char('#')
+                second()
+                char('@')
+            }
+        }
+        blackhole.consume(format)
+    }
+}
