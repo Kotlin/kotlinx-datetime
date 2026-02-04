ef1278ca8f84ab942bd2a3f36886af53e9aa54a2
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2026-02-04 16:09:18 +0100

    Add a comment about binary compatibility

--- core/jvm/src/Instant.kt
+++ core/jvm/src/Instant.kt
@@ -9,6 +9,11 @@ package kotlinx.datetime
 
 import kotlin.time.Instant
 
+/**
+ * All of these functions were available in the `InstantJvm.kt` file as `actual` parts of `expect`/`actual`.
+ * To preserve binary compatibility with 0.7.x, they are left here for one major version (to be removed in 0.9.0).
+ */
+
 @JvmName("minus") @PublishedApi internal fun minusJvm(instant: Instant, value: Int, unit: DateTimeUnit, timeZone: TimeZone): Instant =
     instant.minus(value, unit, timeZone)
 
